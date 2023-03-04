1fe46bef29a096284ed981020621164d508005e0
Milton Cavalcante <milton.hneto@gmail.com>
2023-03-04 06:28:31 -0300

    Projeto iniciado

---
+++ src/main/java/com/miltcn/sosti/SostiApplication.java
@@ -0,0 +1,13 @@
+package com.miltcn.sosti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SostiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SostiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miltcn/sosti/SostiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miltcn.sosti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SostiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
