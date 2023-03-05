16285ce45c5fa6fd8c22df4327f4c0394076881f
Milton Cavalcante <milton.hneto@gmail.com>
2023-03-05 12:38:25 -0300

    Criado endpoint findById para Técnico

---
+++ src/main/java/com/miltcn/sosti/services/TechnicianService.java
@@ -0,0 +1,19 @@
+package com.miltcn.sosti.services;
+
+import com.miltcn.sosti.domain.Technician;
+import com.miltcn.sosti.repositories.TechnicianRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TechnicianService {
+    @Autowired
+    private TechnicianRepository technicianRepository;
+
+    public Technician findById(Integer id) {
+        Optional<Technician>  obj = technicianRepository.findById(id);
+        return obj.orElse(null);
+    }
+}
