998ab7899a5d842289a4f4ea69bdd8d06accf956
Milton Cavalcante <milton.hneto@gmail.com>
2023-03-06 17:33:35 -0300

    Criado endpoint POST /chamados (create) para Chamado

--- src/main/java/com/miltcn/sosti/controllers/ServiceOrderController.java
+++ src/main/java/com/miltcn/sosti/controllers/ServiceOrderController.java
@@ -5,11 +5,11 @@ import com.miltcn.sosti.domain.dtos.ServiceOrderDTO;
 import com.miltcn.sosti.services.ServiceOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,5 +31,12 @@ public class ServiceOrderController {
         List<ServiceOrder> serviceOrderList = this.serviceOrderService.findAll();
         List<ServiceOrderDTO> serviceOrderDTOList = serviceOrderList.stream().map(serviceOrder -> new ServiceOrderDTO(serviceOrder)).collect(Collectors.toList());
         return ResponseEntity.ok().body(serviceOrderDTOList);
+    }
+
+    @PostMapping
+    public ResponseEntity<ServiceOrderDTO> create(@Valid @RequestBody ServiceOrderDTO serviceOrderDTO) {
+        ServiceOrder serviceOrder = this.serviceOrderService.create(serviceOrderDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(serviceOrder.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
