4c6e6aae41c5990d000783f9eee52c257fba1ffb
Milton Cavalcante <milton.hneto@gmail.com>
2023-03-06 15:26:13 -0300

    Implementado CRUD para clientes - Fixed #6

--- src/main/java/com/miltcn/sosti/domain/Client.java
+++ src/main/java/com/miltcn/sosti/domain/Client.java
@@ -1,12 +1,14 @@
 package com.miltcn.sosti.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.miltcn.sosti.domain.dtos.ClientDTO;
 import com.miltcn.sosti.domain.enums.Profile;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 public class Client extends Person {
@@ -24,6 +26,17 @@ public class Client extends Person {
     public Client(Integer id, String name, String cpf, String email, String password) {
         super(id, name, cpf, email, password);
         addProfile(Profile.CLIENT);
+    }
+
+    public Client(ClientDTO clientDTO) {
+        super();
+        this.id = clientDTO.getId();
+        this.name = clientDTO.getName();
+        this.cpf = clientDTO.getCpf();
+        this.email = clientDTO.getEmail();
+        this.password = clientDTO.getPassword();
+        this.profiles = clientDTO.getProfiles().stream().map(profile -> profile.getCode()).collect(Collectors.toSet());
+        this.createdAt = clientDTO.getCreatedAt();
     }
 
     public List<ServiceOrder> getServiceOrders() {
