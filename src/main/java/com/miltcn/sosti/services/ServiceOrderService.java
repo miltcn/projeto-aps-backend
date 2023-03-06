9242430ace9950a5875bca901d72c54b9b7366d9
Milton Cavalcante <milton.hneto@gmail.com>
2023-03-06 16:15:19 -0300

    Criado endpoint GET /chamados/id (findById) para Chamado

---
+++ src/main/java/com/miltcn/sosti/services/ServiceOrderService.java
@@ -0,0 +1,22 @@
+package com.miltcn.sosti.services;
+
+import com.miltcn.sosti.domain.ServiceOrder;
+import com.miltcn.sosti.repositories.ServiceOrderRepository;
+import com.miltcn.sosti.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
+
+@Service
+public class ServiceOrderService {
+
+    @Autowired
+    private ServiceOrderRepository serviceOrderRepository;
+
+    public ServiceOrder findById(Integer id) {
+        Optional<ServiceOrder> serviceOrder = serviceOrderRepository.findById(id);
+        return serviceOrder.orElseThrow(() -> new ObjectNotFoundException("Chamado não encontrado! ID: " + id));
+    }
+}
