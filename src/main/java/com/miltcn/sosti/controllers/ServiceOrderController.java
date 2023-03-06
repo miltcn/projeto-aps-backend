841c9414aa596db174aa2052d5064194cbbb8b84
Milton Cavalcante <milton.hneto@gmail.com>
2023-03-06 17:01:31 -0300

    Criado endpoint GET /chamados (findAll) para Chamado

--- src/main/java/com/miltcn/sosti/controllers/ServiceOrderController.java
+++ src/main/java/com/miltcn/sosti/controllers/ServiceOrderController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/chamados")
 public class ServiceOrderController {
@@ -21,5 +24,12 @@ public class ServiceOrderController {
     public ResponseEntity<ServiceOrderDTO> findById(@PathVariable Integer id) {
         ServiceOrder serviceOrder = this.serviceOrderService.findById(id);
         return ResponseEntity.ok().body(new ServiceOrderDTO(serviceOrder));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ServiceOrderDTO>> findAll() {
+        List<ServiceOrder> serviceOrderList = this.serviceOrderService.findAll();
+        List<ServiceOrderDTO> serviceOrderDTOList = serviceOrderList.stream().map(serviceOrder -> new ServiceOrderDTO(serviceOrder)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(serviceOrderDTOList);
     }
 }
--- src/main/java/com/miltcn/sosti/services/ServiceOrderService.java
+++ src/main/java/com/miltcn/sosti/services/ServiceOrderService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,5 +19,9 @@ public class ServiceOrderService {
     public ServiceOrder findById(Integer id) {
         Optional<ServiceOrder> serviceOrder = serviceOrderRepository.findById(id);
         return serviceOrder.orElseThrow(() -> new ObjectNotFoundException("Chamado não encontrado! ID: " + id));
+    }
+
+    public List<ServiceOrder> findAll() {
+        return this.serviceOrderRepository.findAll();
     }
 }
